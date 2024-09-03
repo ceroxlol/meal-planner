a624fed734947eabcc7bdb5488996985c73b8049
Christopher Werner <christopher.werner1@gmx.net>
2024-09-03 15:44:55 +0200

    Remove unused component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MealListComponent } from './components/meal-list/meal-list.component';
-import { MealDetailsComponent } from './components/meal-details/meal-details.component';
 
 export const routes: Routes = [
   { path: '', component: MealListComponent },
-  { path: 'meal-details/:id', component: MealDetailsComponent }
 ];
 
 @NgModule({
--- src/app/components/meal-list/meal-list.component.ts
+++ src/app/components/meal-list/meal-list.component.ts
@@ -15,25 +15,7 @@ import { MealService } from '../../services/meal.service';
   imports: [MatListModule, CommonModule, MatCardModule]
 })
 export class MealListComponent {
-  meals: Meal[] = [
-    {
-      id: 0,
-      title: 'Spaghetti Bolognese',
-      ingredients: ['Spaghetti' , 'Tomato Sauce', 'Ground Beef' ],
-      timeToCook: 45,
-      effortLevel: 3,
-      imageUrl: 'spaghettiBolognese.webp'
-    },
-    {
-      id: 1,
-      title: 'Caesar Salad',
-      ingredients: ['Lettuce','Croutons','Caesar Dressing' ],
-      timeToCook: 20,
-      effortLevel: 1,
-      imageUrl: 'caesarSalad.webp'
-    }
-    // Add more meals as needed
-  ];
+  meals: Meal[] = [];
 
   @Output() mealSelected = new EventEmitter<Meal>();
 
