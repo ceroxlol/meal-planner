09ae41756a762d1fb42bc88e7d412726618682ba
Christopher Werner <christopher.werner1@gmx.net>
2024-12-31 14:20:05 +0100

    Adjust layout.

--- src/app/meal.model.ts
+++ src/app/meal.model.ts
@@ -2,7 +2,7 @@ export interface Meal {
   _id: string;
   title: string;
   ingredients: string[];
-  timeToCook: number; // in minutes
+  cookingTime: number; // in minutes
   effortLevel: number; // 1 to 3
   imageUrl: string;
 }
